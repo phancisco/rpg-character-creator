3c7a44c297fc84386579ff9fbd409b8f152fc4a3
phanxico <caniunirjfco@gmail.com>
2025-05-16 03:04:20 -0400

    holaaaa

--- RpgCharacterCreator/settings.py
+++ RpgCharacterCreator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ STATIC_DIR = BASE_DIR / 'static'
 SECRET_KEY = 'django-insecure-tgk$lw1ax)xz3&9n#h=&6)4wb52h7byyi5+8p7(nn&$r&idam@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,6 +119,11 @@ USE_TZ = True
 STATICFILES_DIRS = [
     STATIC_DIR,]
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
